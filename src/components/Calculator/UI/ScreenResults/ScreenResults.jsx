4ee2c654846e61548683999f3d7ff2a58b375daa
StormySilverio05 <stormysilverionunez@gmail.com>
2024-03-31 14:49:12 -0400

    [SC-4]: Screen Results, Creation of Screen Results

---
+++ src/components/Calculator/UI/ScreenResults/ScreenResults.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ScreenResults() {
+  return (
+    <div className='Display'>
+        <div className='OperationHistorial'>4096 - 2252=</div>
+        <div className='ResultHistorial'>1844</div>
+        <div className='MainDisplay'>1200+400=</div>
+        <div className='Result'>1400</div>
+    </div>
+  )
+}
+
+export default ScreenResults
